06adf88b2144dd72087049dc5de84c8acb168d5f
joyW16 <wellen.joyce3@gmail.com>
2026-08-25 16:40:53 +0000

    exercicios

---
+++ aula_10/index.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from retanguloUI import RetanguloUI
+
+#st.header("POO em Python com Stramlit")
+
+#if st.button("Clique Aqui"):
+    #st.write("Bem-vindo(a) ao Streamlit")
+
+RetanguloUI.main()
